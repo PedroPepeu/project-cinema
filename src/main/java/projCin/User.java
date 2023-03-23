e5f6d797e743ec314f07bcc258e4817c64faeb5e
Pepeu <pedrosteamgtav@gmail.com>
2023-03-23 14:51:23 -0300

    tests added, declared some variables in user

---
+++ src/main/java/projCin/User.java
@@ -0,0 +1,16 @@
+package main.java.projCin;
+
+public class User {
+    
+    public String user;
+    public int[] cpf;
+    public int password;
+    public int age;
+    public char gender;
+    public String email;
+    public String creditCardName;
+    public int[] creditCardNum;
+    public int creditCardVerify;
+
+
+}

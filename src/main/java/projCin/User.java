473347cfd8d410b3097c91fa5939911b01f9c0f7
Rian Delou <riandelou63@hotmail.com>
2023-03-30 19:41:28 -0300

    classe user feita

--- src/main/java/projCin/User.java
+++ src/main/java/projCin/User.java
@@ -2,17 +2,93 @@ package projCin;
 
 public class User {
     
-    public String user;
-    public int[] cpf;
-    public int password;
-    public int age;
-    public char gender;
-    public String email;
-    public String creditCardName;
-    public int[] creditCardNum;
-    public int creditCardVerify;
+    private String user;
+    private int[] cpf;
+    private int password;
+    private int age;
+    private char gender;
+    private String email;
+    private String creditCardName;
+    private int[] creditCardNum;
+    private int creditCardVerify;
 
     public User() {
+        
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int[] getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int[] cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCreditCardName() {
+        return creditCardName;
+    }
+
+    public void setCreditCardName(String creditCardName) {
+        this.creditCardName = creditCardName;
+    }
+
+    public int[] getCreditCardNum() {
+        return creditCardNum;
+    }
+
+    public void setCreditCardNum(int[] creditCardNum) {
+        this.creditCardNum = creditCardNum;
+    }
+
+    public int getCreditCardVerify() {
+        return creditCardVerify;
+    }
+
+    public void setCreditCardVerify(int creditCardVerify) {
+        this.creditCardVerify = creditCardVerify;
+    }
+
+    public static void main(String[] args) {
         
     }
 

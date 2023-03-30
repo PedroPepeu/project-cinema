3f93759078693609adbd9c6767c4d668e16220fe
Rian Delou <riandelou63@hotmail.com>
2023-03-30 20:14:42 -0300

    adicionando uns detalhes na classe user, fazendo gets e sets da classe movie e ticket

--- src/main/java/projCin/Movie.java
+++ src/main/java/projCin/Movie.java
@@ -2,13 +2,54 @@ package projCin;
 
 public class Movie {
 
-    public String name;
-    public int seconds;
-    public String summary; // sinopse
-    public double price;
+    private String name;
+    private int seconds;
+    private String summary; // sinopse
+    private double price;
 
     public Movie() {
 
     }
+
+    public Movie(String name, int seconds, String summary, double price) {  //fazendo uma sobrecarga de construtures para ter uma com os parametros e outra sem nenhum parametro
+        this.name = name;
+        this.seconds = seconds;
+        this.summary = summary;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
 
 }
--- src/main/java/projCin/Ticket.java
+++ src/main/java/projCin/Ticket.java
@@ -2,16 +2,86 @@ package projCin;
 
 public class Ticket{
     
-    public String user;
-    public int[] CPF;
-    public Room room;
-    public MovieTime movietime;
-    public String movie;
-    public double valor;
-    public String chair;
+    private String user;
+    private int[] CPF;
+    private Room room;
+    private MovieTime movietime;
+    private String movie;
+    private double valor;
+    private String chair;
 
     public Ticket() {
         
     }
+
+    public Ticket(String user, int[] CPF, Room room, MovieTime movietime, String movie, double valor, String chair) { //fazendo uma sobrecarga de construtures para ter uma com os parametros e outra sem nenhum parametro
+        this.user = user;
+        this.CPF = CPF;
+        this.room = room;
+        this.movietime = movietime;
+        this.movie = movie;
+        this.valor = valor;
+        this.chair = chair;
+    }
+
+
+
+    public String getUser() { 
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int[] getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int[] CPF) {
+        this.CPF = CPF;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public MovieTime getMovietime() {
+        return movietime;
+    }
+
+    public void setMovietime(MovieTime movietime) {
+        this.movietime = movietime;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getChair() {
+        return chair;
+    }
+
+    public void setChair(String chair) {
+        this.chair = chair;
+    }
+
+    
 
 }

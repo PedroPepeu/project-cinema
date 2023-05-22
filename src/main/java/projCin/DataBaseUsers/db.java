a284bd42a3c94a7903d2265a2f52058597b81c72
PedroPepeu <pedrosteamgtav@gmail.com>
2023-05-21 21:10:30 -0300

    database done, just need to add the delete

--- src/main/java/projCin/DataBaseUsers/db.java
+++ src/main/java/projCin/DataBaseUsers/db.java
@@ -1,29 +1,105 @@
+package projCin.DataBaseUsers;
+
 import java.io.*; // the class that create the file java.io.file
 import java.util.*;
 
 public class db {
 
-    public static void main(String args[]) {
-        
+    public void insert() {
+
         Scanner sc = new Scanner(System.in);
+
+        System.out.printf("ID..\n");
+        String ID = sc.nextLine();
         
-        System.out.println("name..");
+        System.out.printf("name..\n");
         String name = sc.nextLine();
 
-        System.out.println("password..");
+        System.out.printf("password..\n");
         String pssw = sc.nextLine();
 
-        System.out.println("email..");
+        System.out.printf("email..\n");
         String email = sc.nextLine();
 
         try {
-            File file = new File("Database.txt");
+            File file = new File("./src/main/java/projCin/DataBaseUsers/Database.txt");
             PrintWriter pw = new PrintWriter(new FileOutputStream(file, true));
-            pw.append("{\n");
-            pw.append("\t"+name+"\n"+"\t"+email+"\n"+"\t"+pssw+"\n");
-            pw.append("}\n");
+            pw.append(ID+","+name+","+email+","+pssw+",\n");
             pw.close();
         } catch (Exception e) {}
 
+        sc.close();
+    }
+
+    public void updateFile() {
+
+        int numOfValues = 3;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("Write the ID that you whant to change..\n");
+        int idToChange = sc.nextInt();
+
+        String jumpline = sc.nextLine();
+
+        String changingArr[] = {"Name..", "email..", "password.."};
+
+        int willChange[] = new int[numOfValues];
+        String willUpdate[] = new String[numOfValues];
+
+        System.out.printf("Select the column that you want to update:\n(0 to NO, 1 to YES)\n");
+        for(int i = 0; i < numOfValues; i++) {
+            System.out.printf("%s", changingArr[i]);
+            int option = sc.nextInt();
+            willChange[i] = option;
+        }
+
+        Scanner sc1 = new Scanner(System.in);
+
+        System.out.printf("\n\n\nAdd new values to the columns\n");
+        for(int i = 0; i < numOfValues; i++) {
+            System.out.printf("%s\n", changingArr[i]);
+            if(willChange[i] == 1) {
+                String strToUpdate = sc1.nextLine();
+                willUpdate[i] = strToUpdate;
+            } else {
+                System.out.printf("Not selected as changable\n");
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("./src/main/java/projCin/DataBaseUsers/Database.txt"));
+            String s = "";
+            while((s = br.readLine()) != null) {
+                String data[] = new String[numOfValues + 1];
+                data = s.split(",");
+                if(idToChange == Integer.parseInt(data[0])) {
+                    String row = data[0] + ",";
+                    for(int i = 0; i < numOfValues; i++) {
+                        if(willChange[i] == 1) {
+                            row = row + willUpdate[i]+",";
+                        } else {
+                            row = row + data[i + 1]+",";
+                        }
+                    }
+                    sb.append(row);
+                    sb.append("\n");
+                } else {
+                    sb.append(s);
+                    sb.append("\n");
+                }
+            }
+            // System.out.printf("%s\n", sb.toString());
+
+            File file = new File("./src/main/java/projCin/DataBaseUsers/Database.txt");
+            PrintWriter pw = new PrintWriter(new FileOutputStream(file, false));
+            pw.print(sb.toString());
+            pw.close();
+        
+        } catch (Exception e) {}
+        
+        sc1.close();
+        sc.close();
     }
 }
